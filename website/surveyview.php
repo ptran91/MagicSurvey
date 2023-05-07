60f57b6e6289f5a4fdd13673c8dd6c181f4badb5
Citryte <citryte@outlook.com>
2023-05-07 02:03:39 -0700

    Added minor functionality to the search page

--- website/surveyview.php
+++ website/surveyview.php
@@ -1,6 +1,27 @@
 <?php 
     session_start();
-    
+    $message = "Results";
+    if(isset($_POST["btn_search"])) {
+        if(isset($_POST["id_search"]) && isset($_POST["name_search"])) {
+            $search_id = $_POST["id_search"];
+            $search_name = $_POST["name_search"];
+
+            $search_id = strip_tags($search_id);
+            $search_id = addslashes($search_id);
+            $search_name = strip_tags($search_name);
+            $search_name = addslashes($search_name);
+            
+            if($search_id == "" && $search_name == "") {
+                $message = "There are no search terms!";
+            } else if($search_id == "") {
+                $message = "Surveys with name $search_name";
+            } else if($search_name == "") {
+                $message = "Surveys with ID $search_id";
+            } else {
+                $message = "Surveys with ID $search_id and name $search_name";
+            }
+        }
+    }
 ?>
     <form method="POST" action="surveyview.php">
     <fieldset>
@@ -8,11 +29,11 @@
 	    	<table>
 	    		<tr>
 	    			<td>Search by ID</td>
-	    			<td><input type="text" name="id_search" size="30"></td>
+	    			<td><input type="search" name="id_search" size="30"></td>
 	    		</tr>
 	    		<tr>
 	    			<td>Search By Name</td>
-	    			<td><input type="text" name="name_search" size="30"></td>
+	    			<td><input type="search" name="name_search" size="30"></td>
 	    		</tr>
 	    		<tr>
 	    			<td colspan="2" align="center"> <input type="submit" name="btn_search" value="Search"></td>
@@ -21,7 +42,7 @@
   </fieldset>
 
   <fieldset>
-    <legend>Results</legend>
+    <legend><?php echo $message ?> </legend>
             <table>
                 <ul>
                     <li><a href="index.php">Test Link (goes back to index)</a></li>
