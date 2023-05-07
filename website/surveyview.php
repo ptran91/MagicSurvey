468aeaecc47aab380b7990a6e68807e8b604ab18
Citryte <citryte@outlook.com>
2023-05-07 01:21:12 -0700

    Added a survey search page

--- website/surveyview.php
+++ website/surveyview.php
@@ -1,8 +1,31 @@
 <?php 
     session_start();
-    if(isset($_SESSION["username"])){
-        $username = $_SESSION["username"];
-    }
+    
 ?>
     <form method="POST" action="surveyview.php">
+    <fieldset>
+	    <legend>Find Surveys</legend>
+	    	<table>
+	    		<tr>
+	    			<td>Search by ID</td>
+	    			<td><input type="text" name="id_search" size="30"></td>
+	    		</tr>
+	    		<tr>
+	    			<td>Search By Name</td>
+	    			<td><input type="text" name="name_search" size="30"></td>
+	    		</tr>
+	    		<tr>
+	    			<td colspan="2" align="center"> <input type="submit" name="btn_search" value="Search"></td>
+	    		</tr>
+	    	</table>
+  </fieldset>
+
+  <fieldset>
+    <legend>Results</legend>
+            <table>
+                <ul>
+                    <li><a href="index.php">Test Link (goes back to index)</a></li>
+				</ul>
+            </table>
+  </fieldset>
     </form>
