607f562085a496d736860d507e68f8d819917a09
ptran91 <ptran91@csu.fullerton.edu>
2023-05-08 01:12:43 -0700

    changes

--- website/login.php
+++ website/login.php
@@ -1,3 +1,21 @@
+<style>
+    body {
+        background-image: url('free/images/hero-bg.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+    .form-container {
+        background-color: white;
+        padding: 20px;
+        border-radius: 10px;
+        width: 500px;
+        margin: auto;
+    }
+    .form-container h2 {
+        text-align: center;
+    }
+</style>
+
 <?php
 require_once("lib/connection.php");
 
@@ -50,49 +68,34 @@ if (isset($_POST["btn_submit"])) {
                 exit;
             }
         }
-        .form-container h2 {
-            text-align: center;
-        }
-    </style>
-</head>
+    } else {
+        echo "Username or password is not set!";
+    }
+}
+?>
+
 <html>
 <head>
     <title>Magic Survey</title>
 </head>
 <body>
 <div class="form-container">
-    <h2>Please register here!</h2>
-    <form action="register.php" method="post">
+    <h2>
+        Please log in here!
+    </h2>
+    <form method="POST" action="login.php">
         <table>
             <tr>
-                <td>Username:</td>
-                <td><input type="text" id="username" name="username"></td>
+                <td>Username</td>
+                <td><input type="text" name="username" size="30"></td>
             </tr>
             <tr>
-                <td>Password:</td>
-                <td><input type="password" id="pass" name="pass"></td>
+                <td>Password</td>
+                <td><input type="password" name="pass" size="30"></td>
             </tr>
             <tr>
-                <td>First Name:</td>
-                <td><input type="text" id="firstname" name="firstname"></td>
-            </tr>
-            <tr>
-                <td>Last Name:</td>
-                <td><input type="text" id="lastname" name="lastname"></td>
-            </tr>
-            <tr>
-                <td>Phone Number:</td>
-                <td><input type="text" id="phone" name="phone"></td>
-            </tr>
-            <tr>
-                <td>Email:</td>
-                <td><input type="text" id="email" name="email"></td>
-            </tr>
-            <tr>
-                <td colspan="2" align="center"><input type="submit" name="btn_submit" value="Register"></td>
+                <td colspan="2" align="center"><input type="submit" name="btn_submit" value="Log in"></td>
             </tr>
         </table>
     </form>
 </div>
-</body>
-</html>
