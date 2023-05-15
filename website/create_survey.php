a5304ae848d4f670e3ba2e6d87246130482eba82
Daniel Currey <spiceycurrey@gmail.com>
2023-05-14 21:41:46 -0700

    Modified create_survey

--- website/create_survey.php
+++ website/create_survey.php
@@ -1,33 +1,35 @@
 <?php
-// Include the connection file and any other necessary dependencies
 require_once("lib/connection.php");
+session_start();
 
-// Check if the form is submitted
 if (isset($_POST["btn_submit"])) {
-    // Retrieve the form data
     $code = $_POST["SurveyCode"];
     $name = $_POST["name"];
     $description = $_POST["description"];
     $startDateTime = $_POST["StartDateTime"];
     $endDateTime = $_POST["EndDateTime"];
-    $userId = $_POST["userId"];
-    $statusId = $_POST["statusId"];
+    $userId = $_SESSION["user_id"];
 
-    // Validate the form data (perform any necessary validation checks)
+    // Create survey status
+    $statusData = array(
+        'statusName' => 'Active',
+        'statusTimeStamp' => date('Y-m-d H:i:s')
+    );
 
-    // Create an array to hold the questions
+    $statusId = create_status($conn, $statusData['statusName'], $statusData['statusTimeStamp']);
+
+    // Validate the form data
+
     $questions = [];
 
-    // Retrieve the question data from the form
     $questionCount = $_POST["question_count"];
     for ($i = 1; $i <= $questionCount; $i++) {
         $questionName = $_POST["question_name_" . $i];
         $questionDescription = $_POST["question_description_" . $i];
         $questionType = $_POST["question_type_" . $i];
 
-        // Validate the question data (perform any necessary validation checks)
+        // Validate the question data
 
-        // Add the question to the questions array
         $questions[] = [
             "name" => $questionName,
             "description" => $questionDescription,
@@ -35,32 +37,47 @@ if (isset($_POST["btn_submit"])) {
         ];
     }
 
-    // Call the create_survey function passing the form data
     create_survey($conn, $code, $name, $description, $startDateTime, $endDateTime, $questions, $userId, $statusId);
 }
 
+function create_status($conn, $statusName, $statusTimeStamp)
+{
+    try {
+        $stmt = $conn->prepare("INSERT INTO statuses (Name, timestamp_date) VALUES (?, ?)");
+        $stmt->execute([$statusName, $statusTimeStamp]);
+
+        return $conn->lastInsertId();
+    } catch (PDOException $e) {
+        // Handle the exception here
+        echo "Error creating status: " . $e->getMessage();
+        exit();
+    }
+}
+
+
 function create_survey($conn, $code, $name, $description, $startDateTime, $endDateTime, $questions, $userId, $statusId)
 {
-    // Insert survey details into the database
-    $stmt = $conn->prepare("INSERT INTO surveys (SurveyCode, name, description, StartDateTime, EndDateTime, UserId, StatusId) VALUES (?, ?, ?, ?, ?, ?, ?)");
-    $stmt->execute([$code, $name, $description, $startDateTime, $endDateTime, $userId, $statusId]);
+    try {
+        $stmt = $conn->prepare("INSERT INTO surveys (SurveyCode, name, description, StartDateTime, EndDateTime, UserId, StatusId) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $stmt->execute([$code, $name, $description, $startDateTime, $endDateTime, $userId, $statusId]);
 
-    // Get the ID of the newly inserted survey
-    $surveyId = $conn->lastInsertId();
+        $surveyId = $conn->lastInsertId();
 
-    // Insert questions into the database
-    foreach ($questions as $position => $question) {
-        $questionName = $question['name'];
-        $questionDescription = $question['description'];
-        $questionType = $question['type'];
+        foreach ($questions as $position => $question) {
+            $questionName = $question['name'];
+            $questionDescription = $question['description'];
+            $questionType = $question['type'];
 
-        // Execute SQL query to insert the question
-        $stmt = $conn->prepare("INSERT INTO questions (survey_id, name, description, type, position) VALUES (?, ?, ?, ?, ?)");
-        $stmt->execute([$surveyId, $questionName, $questionDescription, $questionType, $position]);
+            $stmt = $conn->prepare("INSERT INTO questions (survey_id, name, description, type, position) VALUES (?, ?, ?, ?, ?)");
+            $stmt->execute([$surveyId, $questionName, $questionDescription, $questionType, $position]);
+        }
+
+        header("Location: index.php");
+        exit();
+    } catch (PDOException $e) {
+        // Handle the exception here
+        echo "Error creating survey: " . $e->getMessage();
+        exit();
     }
-
-    // Redirect to a success page or do any additional processing if needed
-    header("Location: index.php");
-    exit();
 }
 ?>
--- website/survey_form.php
+++ website/survey_form.php
@@ -15,36 +15,14 @@
 
     <label for="EndDateTime">End Date and Time:</label>
     <input type="datetime-local" name="EndDateTime" required><br>
-
-    <input type="hidden" name="UserID" value="<?php echo $_SESSION['UserID']; ?>" required><br>
+    
+    <input type="hidden" name="UserID" value="<?php echo isset($_SESSION['user_id']) ? $_SESSION['user_id'] : ''; ?>" required><br>
     <input type="hidden" name="StatusID" value="1" required><br>
 
     <!-- Questions -->
     <h3>Questions:</h3>
 
-    <div id="questions_container">
-        <?php
-        if (isset($_POST['question_count'])) {
-            $questionCount = $_POST['question_count'];
-            // Generate input fields for each question
-            for ($i = 1; $i <= $questionCount; $i++) {
-                echo '<h4>Question ' . $i . '</h4>';
-                echo '<label for="question_name_' . $i . '">Question Name:</label>';
-                echo '<input type="text" name="question_name_' . $i . '" required><br>';
-
-                echo '<label for="question_description_' . $i . '">Question Description:</label>';
-                echo '<textarea name="question_description_' . $i . '" required></textarea><br>';
-
-                echo '<label for="question_type_' . $i . '">Question Type:</label>';
-                echo '<select name="question_type_' . $i . '" required>';
-                echo '<option value="text">Text</option>';
-                echo '<option value="radio">Radio</option>';
-                echo '<option value="checkbox">Checkbox</option>';
-                echo '</select><br>';
-            }
-        }
-        ?>
-    </div>
+    <div id="questions_container"></div>
 
     <button type="button" id="add_question">Add Question</button>
 
@@ -58,11 +36,12 @@
     var questionsContainer = document.getElementById('questions_container');
     var questionCountInput = document.getElementById('question_count');
 
-    document.getElementById('add_question').addEventListener('click', function () {
+    function addQuestion(isRemovable) {
         questionCount++;
 
         var questionDiv = document.createElement('div');
-        questionDiv.innerHTML = '<h4>Question ' + questionCount + '</h4>' +
+        questionDiv.classList.add('question');
+        questionDiv.innerHTML = '<h4 class="question-number">Question ' + questionCount + '</h4>' +
             '<label for="question_name_' + questionCount + '">Question Name:</label>' +
             '<input type="text" name="question_name_' + questionCount + '" required><br>' +
             '<label for="question_description_' + questionCount + '">Question Description:</label>' +
@@ -72,24 +51,46 @@
             '<option value="text">Text</option>' +
             '<option value="radio">Radio</option>' +
             '<option value="checkbox">Checkbox</option>' +
-            '</select>' +
-            '<button type="button" class="remove_question">Remove Question</button><br>';
+            '</select>';
+
+        if (isRemovable) {
+            var removeButton = document.createElement('button');
+            removeButton.type = 'button';
+            removeButton.innerText = 'Remove Question';
+            removeButton.addEventListener('click', function () {
+                questionDiv.remove();
+                questionCount--;
+                updateQuestionNumbers();
+
+                // Update the question count input value
+                questionCountInput.value = questionCount;
+
+                // If the number of questions is less than 5, add a new question
+                if (questionCount < 5) {
+                    addQuestion(false);
+                }
+            });
+            questionDiv.appendChild(removeButton);
+        }
 
         questionsContainer.appendChild(questionDiv);
 
         // Update the question count input value
         questionCountInput.value = questionCount;
+    }
 
-        // Add event listener to the remove question button
-        var removeQuestionButtons = document.getElementsByClassName('remove_question');
-        for (var i = 0; i < removeQuestionButtons.length; i++) {
-            removeQuestionButtons[i].addEventListener('click', function () {
-                this.parentNode.remove();
-                questionCount--;
-
-                // Update the question count input value
-                questionCountInput.value = questionCount;
-            });
+    function updateQuestionNumbers() {
+        var questions = document.getElementsByClassName('question');
+        for (var i = 0; i < questions.length; i++) {
+            var questionNumberElement = questions[i].getElementsByClassName('question-number')[0];
+            questionNumberElement.innerText = 'Question ' + (i + 1);
         }
-    });
+    }
+
+    document.getElementById('add_question').addEventListener('click', function () { addQuestion(true); });
+
+    // Add 5 questions initially
+    for (var i = 0; i < 5; i++) {
+        addQuestion(false);
+    }
 </script>
